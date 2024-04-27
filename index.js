db89542dd21ef5a64adbff14a5f35a481b270509
Alvaro Escudero <135117878+Alvaroescumed@users.noreply.github.com>
2024-04-27 20:07:18 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// importamos el paquete http
+// importamos el modulo http
 const http = require('http')
-//importamos el paquete fs para realizar el fecth de los datos
+//importamos el modulo fs para realizar el fecth de los datos
 const fs = require('fs')
 
 //creamos una funcion asincrona en la que fetcheamos los datos del JSON de Pokemons
@@ -17,29 +17,49 @@ const fetchPokeData = async () => {
 }
 
 //Creaamos otra función asincrona que manje las peticiones
-const handleRequest = async(request, res) => {
+const handleRequest = async(req, res) => {
 
-    const pokeData = await fetchPokeData;
-    const pokemons = decodeURI(request.url.substring(1))
+    const pokeData = await fetchPokeData();
+    const pokemonName = decodeURI(req.url.substring(1))
 
-    const characteristics= pokeData.find( pokemon => pokemon.name === pokemon)
+    const pokemons = pokeData.find( pokemon => {
+        
+        // creamos un bucle for en el que comprobaremos todos los idiomas del pokemon
+        for ( language in pokemon.name){
+            if(pokemon.name[language] === pokemonName){
+                return true
+            } 
+        }
+        // una vez comprobado el idioma comprobamos si el ID del pokemon
+        if(pokemon.id.toString() === pokemonName){
+            return true
+        }
 
-    if(characteristics){
+        return false
+
+    })
+
+    if(pokemons){
         const response = {
             'Tipo': pokemons.type,
             'HP': pokemons.base.HP,
             'Attack': pokemons.base.Attack,
             'Defense': pokemons.base.Defense,
-            'Sp. Attack': pokemons.base.Sp. Attack,
-            'Sp. Defense': pokemons.base.Sp. Defense,
+            'Sp. Attack': pokemons.base['Sp. Attack'],
+            'Sp. Defense': pokemons.base['Sp. Defense'],
             'Speed': pokemons.base.Speed
         }
 
+        const responseJSON = JSON.stringify(response)
+        
         res.writeHead(200, {'Content-type': 'application/json'});
-        res.end(JSON.stringify(response, null, 4))
+        res.end(responseJSON)
+
     }else{
+
         res.writeHead(404, {'Content-Type': 'text/plain'})
         res.end('El pokemon que busca no existe, introducza un nombre o id correcto')
+        
     }
 
 }
